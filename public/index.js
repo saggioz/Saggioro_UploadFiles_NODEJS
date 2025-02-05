347c3e3704813f8b6232b11a0246564d8ee1e031
FEDERICO <federico.saggiro@icloud.com>
2025-02-05 22:21:08 +0100

    fix

--- public/index.js
+++ public/index.js
@@ -1,26 +1,35 @@
-(async () => {
-    const inputFile = document.querySelector('#file');
-    const button = document.querySelector("#button");
-    const link = document.querySelector("#link");
-  
-    handleSubmit = async (event) => {
-        const formData = new FormData();
-        formData.append("file", inputFile.files[0]);
-        const body = formData;
-        body.description = inputDescription.value;
-        const fetchOptions = {
-            method: 'post',
-            body: body
-        };
-        try {
-            const res = await fetch("/upload", fetchOptions);
-            const data = await res.json();
-            link.setAttribute("href", data.url);
-            link.innerText = data.url;
-        } catch (e) {
-            console.log(e);
-        }
-    }
+const inputFile = document.querySelector('#file');
+const button = document.querySelector("#button");
+const link = document.querySelector("#link");
 
-    button.onclick = handleSubmit;
-})();
+const render = async () => {
+    const list = await fetch("/filelist");
+    const data = await list.json();
+
+    const html = data
+        .map(e => `<li><a href="${e}">${e}</a></li>`)
+        .join("");
+
+    link.innerHTML = `<ul>${html}</ul>`; 
+};
+
+await render();
+
+const handleSubmit = async (event) => {
+    const formData = new FormData();
+    formData.append("file", inputFile.files[0]);
+    const body = formData;
+    const fetchOptions = {
+        method: 'post',
+        body: body
+    };
+    try {
+        const res = await fetch("/upload", fetchOptions);
+        const data = await res.json();
+        await render();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+button.onclick = handleSubmit;
